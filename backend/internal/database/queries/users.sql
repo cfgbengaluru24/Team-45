8285afb63e5ccbafaa7ebedce98b4c78de3060ac
harsh_dadwal <harshdadymon@gmail.com>
2024-07-28 02:23:46 +0530

    Added Registration Queries

--- backend/internal/database/queries/users.sql
+++ backend/internal/database/queries/users.sql
@@ -1,3 +1,19 @@
 -- name: GetAllUsers :many
 Select id, email, full_name, phone, role
 from users;
+
+-- name: RegisterUser :one
+Insert into users(email,full_name,phone,"role",password_hash) values ($1,$2,$3,$4,$5) returning *;
+
+-- name: RegisterSchool :exec
+Insert into schools(id,"name","location","status") values ($1,$2,$3,$4); 
+
+-- name: RegisterGrassroots :exec
+Insert into grassroots(id,"location") values ($1,$2);
+
+-- name: RegisterDonors :exec
+Insert into donors(id,"anonymous",get_report,donated) values ($1,$2,$3,$4);
+
+
+
+
