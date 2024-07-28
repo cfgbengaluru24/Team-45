b7e6175ea410c1b5b3b949e1aa3c84ae63660504
Aditya-Chowdhary <adityachowdhary04@gmail.com>
2024-07-28 05:51:49 +0530

    remaining endpoints

--- backend/internal/database/queries/users.sql
+++ backend/internal/database/queries/users.sql
@@ -27,37 +27,52 @@ Select school_uuid,"name","location",email,phone from
 schools INNER JOIN users ON users.id=school.id  where status=3;
 
 -- name: AdminRequestSchools1 :many
-Select school_uuid,request_id,"name","location","type","details","cost","created_at" from 
+Select schools.school_uuid,request_id,"name","location","type","details","cost","created_at" from 
 requests INNER JOIN schools ON requests.school_uuid=schools.school_uuid 
 INNER JOIN users ON schools.id=users.id where requests.status=1;
 
 -- name: AdminRequestSchools3 :many
-Select school_uuid,request_id,"name","location","type","details","cost","created_at" from 
+Select schools.school_uuid,request_id,"name","location","type","details","cost","created_at" from 
 requests INNER JOIN schools ON requests.school_uuid=schools.school_uuid 
 INNER JOIN users ON schools.id=users.id where requests.status=3;
 
 -- name: GrassrootsGetSchools :many
-Select school_uuid,"name","location",email,phone from 
-schools INNER JOIN users ON users.id=school.id  where status=2;
+Select school_uuid,"name","location",email,phone
+from schools INNER JOIN users ON users.id=school.id
+where status=2 and schools.assigned_grassroot = $1;
 
 -- name: GrassrootsGetRequests :many
-Select school_uuid,request_id,"name","location","type","details","cost","created_at" from 
-requests INNER JOIN schools ON requests.school_uuid=schools.school_uuid 
-INNER JOIN users ON schools.id=users.id where requests.status=2;
+Select schools.school_uuid,request_id,"name","location","type","details","cost","created_at"
+from requests
+INNER JOIN schools ON requests.school_uuid=schools.school_uuid 
+INNER JOIN users ON schools.id=users.id where requests.status=2 and requests.assigned_grassroot = $1;
 
--- name: AdminSchoolVerify :exec
+-- name: UpdateSchoolStatus :exec
 Update schools set status=$2 where school_uuid=$1;
 
--- name: AdminRequestVerify :exec
-Update requests set status=$2 where school_uuid=$1;
+-- name: UpdateRequestStatus :exec
+Update requests set status=$2 where request_id=$1;
 
--- name: GrassrootsSchoolVerify :exec
-Update schools set status=$2 where school_uuid=$1;
+-- name: AssignGrassrootToRequest :exec
+UPDATE requests
+SET assigned_grassroot = $1 
+WHERE request_id = $2;
 
--- name: GrassrootsRequestVerify :exec
-Update requests set status=$2 where school_uuid=$1;
+-- name: AssignGrassrootToSchool :exec
+UPDATE schools
+SET assigned_grassroot = $1 
+WHERE school_uuid = $2;
 
+-- name: CreateDonation :exec
+INSERT INTO donations (donor_uuid, amount, request_id)
+values ($1, $2, $3);
 
+-- name: UpdateTotalDonation :exec
+UPDATE donors
+SET donated = donated + $1
+WHERE donor_uuid = $2;
 
-
-
+-- name: UpdateRequestAfterDonation :exec
+UPDATE requests
+SET donated = donated + $1, status = 6
+WHERE request_id = $2;
